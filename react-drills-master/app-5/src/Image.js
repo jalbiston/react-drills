d6e1af460279c20d03234e5b4658af37fe7065cb
jalbiston <jacob.albiston@gmail.com>
2021-10-22 15:28:30 -0600

    image excercise

---
+++ react-drills-master/app-5/src/Image.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Image extends Component {
+    render() {
+        return (
+            <div>
+                <img 
+                src ="https://static01.nyt.com/images/2021/10/08/world/08skorea-squidgame-biz-inyt-3/06skorea-squidgame-2-mobileMasterAt3x.jpg" />
+            </div>
+        )
+    }
+}
